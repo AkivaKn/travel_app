6ca398d82e7414a982d0da31a195d7b039d8d90b
Joe Mosley <joemosley96@gmail.com>
2024-07-25 12:17:32 +0100

    build: seed database

--- src/app/seed/route.js
+++ src/app/seed/route.js
@@ -12,8 +12,8 @@ async function seedUsers(client) {
   try {
     const createTable = await client.sql`CREATE TABLE users (
               user_id SERIAL PRIMARY KEY,
-              username VARCHAR(20) NOT NULL,
-              email VARCHAR(30) NOT NULL,
+              username VARCHAR(20) NOT NULL UNIQUE,
+              email VARCHAR(30) NOT NULL UNIQUE,
               password VARCHAR NOT NULL,
               bio VARCHAR(1000),
               avatar_img_url VARCHAR
@@ -28,11 +28,6 @@ async function seedUsers(client) {
       })
     );
     console.log(`Seeded ${insertedUsers.length} users`);
-
-    return {
-      createTable,
-      users: insertedUsers,
-    };
   } catch (error) {
     console.error("Error seeding users:", error);
     throw error;
@@ -42,11 +37,12 @@ async function seedUsers(client) {
 async function seedItineraries(client) {
   try {
     const createTable = await client.sql`CREATE TABLE itineraries (
-  itinerary_id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES users(user_id) ON DELETE SET NULL,
-  itinerary_description VARCHAR,
-  created_at TIMESTAMP DEFAULT NOW(),
-  budget INT NOT NULL
+              itinerary_id SERIAL PRIMARY KEY,
+              user_id INT REFERENCES users(user_id) ON DELETE SET NULL,
+              itinerary_description VARCHAR,
+              created_at TIMESTAMP DEFAULT NOW(),
+              budget INT NOT NULL,
+              CHECK (budget BETWEEN 1 AND 3)
 );`;
     console.log("Created itineraries table");
     const insertedItineraries = await Promise.all(
@@ -57,11 +53,6 @@ async function seedItineraries(client) {
       })
     );
     console.log(`Seeded ${insertedItineraries.length} itineraries`);
-
-    return {
-      createTable,
-      itineraries: insertedItineraries,
-    };
   } catch (error) {
     console.error("Error seeding itineraries:", error);
     throw error;
@@ -72,7 +63,7 @@ async function seedDays(client) {
   try {
     const createTable = await client.sql`CREATE TABLE days (
             day_id SERIAL PRIMARY KEY,
-  itinerary_id INT REFERENCES itineraries(itinerary_id) ON DELETE CASCADE NOT NULL,
+            itinerary_id INT REFERENCES itineraries(itinerary_id) ON DELETE CASCADE NOT NULL,
             day_number INT NOT NULL,
             day_plan VARCHAR NOT NULL,
             accomodation VARCHAR(150),
@@ -91,11 +82,6 @@ async function seedDays(client) {
       })
     );
     console.log(`Seeded ${insertedDays.length} days`);
-
-    return {
-      createTable,
-      days: insertedDays,
-    };
   } catch (error) {
     console.error("Error seeding days:", error);
     throw error;
@@ -107,16 +93,20 @@ async function seedComments(client) {
     const createTable = await client.sql`CREATE TABLE comments (
             comment_id SERIAL PRIMARY KEY,
             user_id INT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
-            itineray_id INT REFERENCES itineraries(itinerary_id) ON DELETE CASCADE NOT NULL,
-  comment_body VARCHAR(300),
-  created_at TIMESTAMP DEFAULT NOW()
+            itinerary_id INT REFERENCES itineraries(itinerary_id) ON DELETE CASCADE NOT NULL,
+            comment_body VARCHAR(300),
+            created_at TIMESTAMP DEFAULT NOW()
         );`;
     console.log("Created comments table");
-    //seed with placeholder data
 
-    return {
-      createTable,
-    };
+    const insertedComments = await Promise.all(
+      comments.map(async (comment) => {
+        return client.sql`
+            INSERT INTO comments (user_id, itinerary_id, comment_body, created_at)
+            VALUES (${comment.user_id},  ${comment.itinerary_id}, ${comment.comment_body}, ${comment.created_at});`;
+      })
+    );
+    console.log(`Seeded ${insertedComments.length} comments`);
   } catch (error) {
     console.error("Error seeding comments:", error);
     throw error;
@@ -128,15 +118,19 @@ async function seedItineraryVotes(client) {
     const createTable = await client.sql`CREATE TABLE itinerary_votes (
             vote_id SERIAL PRIMARY KEY,
             user_id INT REFERENCES users(user_id) ON DELETE CASCADE NOT NULL,
-            itineray_id INT REFERENCES itineraries(itinerary_id) ON DELETE CASCADE NOT NULL,
+            itinerary_id INT REFERENCES itineraries(itinerary_id) ON DELETE CASCADE NOT NULL,
             vote_value INT NOT NULL
         );`;
     console.log("Created itinerary_votes table");
-    //seed with placeholder data
 
-    return {
-      createTable,
-    };
+    const insertedItineraryVotes = await Promise.all(
+      itinerary_votes.map(async (itinerary_vote) => {
+        return client.sql`
+            INSERT INTO itinerary_votes (user_id, itinerary_id, vote_value)
+            VALUES (${itinerary_vote.user_id},  ${itinerary_vote.itinerary_id}, ${itinerary_vote.vote_value});`;
+      })
+    );
+    console.log(`Seeded ${insertedItineraryVotes.length} itinerary votes`);
   } catch (error) {
     console.error("Error seeding itinerary_votes:", error);
     throw error;
