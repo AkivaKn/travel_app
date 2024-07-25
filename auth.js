4d2695c77a72c83b4bdfea58ed098714aa974ea5
AkivaKn <akivakaufman@gmail.com>
2024-07-25 15:43:25 +0100

    feat: configure authorisation
    Setup auth.js with credentials provider and create
    a basic signin page.

---
+++ auth.js
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { getUserFromDb } from "@/app/lib/data/users"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null
+ 
+ 
+        // logic to verify if the user exists
+        user = await getUserFromDb(credentials.email, credentials.password)
+ 
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.")
+        }
+        // return user object with their profile data
+        return user
+      },
+    }),
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+          if (user) {
+            token.user = user;
+          }
+          return token;
+        },
+        async session({ session, token }) {
+          if (token.user) {
+            session.user = token.user;
+          }
+          return session;
+        },
+        async redirect({ url, baseUrl }) {
+            return baseUrl
+          },
+      },
+    pages: {
+        signIn: "/signin",
+    },
+})
