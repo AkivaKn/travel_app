f5acfd143eced718c8b897f846142ff70730bc9e
Joe Mosley <joemosley96@gmail.com>
2024-07-27 10:33:46 +0100

    refactor: move data fetching to seperate folder - remove unused imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,15 @@
-import { sql } from "@vercel/postgres";
 import ItineraryCard from "./components/ItineraryCard";
+import { getPopularItineraries } from "./lib/data/itineraries";
 
 export default async function Home() {
-  const res = await sql`
-              SELECT * FROM itineraries;`;
-  const data = res.rows;
+
+  const popularItineraries = await getPopularItineraries()
 
   return (
     <>
       <h1 class='text-center text-3xl m-10'>Home Page</h1>
       <ul>
-        {data.map((itinerary) => (
+        {popularItineraries.map((itinerary) => (
           <li key={itinerary.itinerary_id}>
             <ItineraryCard itinerary={itinerary} />
           </li>
