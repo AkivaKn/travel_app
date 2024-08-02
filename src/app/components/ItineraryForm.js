8ac7db793349cad92cda33e25db2b2ecae28b541
AkivaKn <akivakaufman@gmail.com>
2024-08-02 10:41:13 +0100

    feat: create dynamic form to add itinerary days

--- src/app/components/ItineraryForm.js
+++ src/app/components/ItineraryForm.js
@@ -3,6 +3,7 @@ import { Button } from "flowbite-react";
 import ReactSlider from "react-slider";
 import { generateBudgetString } from "../utils/utils";
 import { useState } from "react";
+import PlacesSelector from "./PlacesSelector";
 
 export default function ItineraryForm() {
   const [budget, setBudget] = useState(1);
@@ -13,7 +14,7 @@ export default function ItineraryForm() {
       transport: "",
       country: "",
       region: "",
-      place: "",
+      city: "",
     },
   ]);
 
@@ -23,12 +24,17 @@ export default function ItineraryForm() {
     setDayInputs(itineraryDays);
   }
   const handleFormChange = (index, event) => {
+    console.log(event);
     let itineraryDays = [...dayInputs];
     itineraryDays[index][event.target.name] = event.target.value;
     setDayInputs(itineraryDays);
   };
 
   function createItinerary(formData) {
+    for (let key of formData) {
+      console.log(key);
+      console.log(dayInputs, "full day inputs");
+    }
     //post data to sql db
   }
 
@@ -39,168 +45,127 @@ export default function ItineraryForm() {
       transport: "",
       country: "",
       region: "",
-      place: "",
+      city: "",
     };
     setDayInputs([...dayInputs, newDayInput]);
   }
 
   return (
-    <div className='h-screen flex max-height:100px max-width:100px m-3 p-3 border-black border-2'>
-      <div className='grow'>
+    <div className="h-screen flex max-height:100px max-width:100px m-3 p-3 border-black border-2">
+      <div className="grow">
         <div>
           <form action={createItinerary}>
             <div>
-              <div className='mb-2 block'>
-                <label htmlFor='title'>Title</label>
+              <div className="mb-2 block">
+                <label htmlFor="title">Title</label>
               </div>
-              <input className='w-full' id='title' name='title' type='text' />
-              {/* {errors.username && <p>{errors.username}</p>} */}
+              <input className="w-full" id="title" name="title" type="text" />
             </div>
             <div>
-              <div className='mb-2 block'>
-                <label htmlFor='itinerary_description'>Description</label>
+              <div className="mb-2 block">
+                <label htmlFor="itineraryDescription">Description</label>
               </div>
               <textarea
-                className='w-full'
-                id='itinerary_description'
-                name='itinerary_description'></textarea>
-              {/* {errors.description && <p>{errors.description}</p>} */}
+                className="w-full"
+                id="itineraryDescription"
+                name="itineraryDescription"
+              ></textarea>
             </div>
-            <div className='my-3'>
-              <div className='mb-2 block'>
-                <label htmlFor='slider'>Budget</label>
+            <div className="my-3">
+              <div className="mb-2 block">
+                <label htmlFor="slider">Budget</label>
               </div>
               <ReactSlider
-                id='slider'
-                className='horizontal-slider'
-                thumbClassName='example-thumb'
-                trackClassName='example-track'
+                id="slider"
+                className="horizontal-slider"
+                thumbClassName="example-thumb"
+                trackClassName="example-track"
                 onAfterChange={(newValue, thumbIndex) => {
                   setBudget(newValue);
                 }}
                 min={1}
                 max={3}
                 renderThumb={(props, state) => (
-                  <div {...props}>
-                    {generateBudgetString(state.valueNow, "$")}
-                  </div>
+                <div {...props} key={1}>
+                  {generateBudgetString(state.valueNow, "$")}
+                </div>
                 )}
               />
-              <input type='hidden' id='budget' name='budget' value={budget} />
-              {/* {errors.password && <p>{errors.password}</p>} */}
+              <input type="hidden" id="budget" name="budget" value={budget} />
             </div>
 
             <div>
-              <div className='mb-2 block'>
-                <label htmlFor='itinerary_image'>
-                  Upload Image (max 4.5mb)
-                </label>
+              <div className="mb-2 block">
+                <label htmlFor="itineraryImage">Upload Image (max 4.5mb)</label>
               </div>
               <input
-                className='w-full'
-                id='itinerary_image'
-                name='itinerary_image'
-                type='file'
-                accept='image/*'
+                className="w-full"
+                id="itineraryImage"
+                name="itineraryImage"
+                type="file"
+                accept="image/*"
               />
             </div>
-
-            <ul className='w-full' id='daysList'>
+            <ul className="w-full" id="daysList">
               {dayInputs.map((day, index) => {
                 return (
-                  <>
-                    <h3>{`Day ${index}`}</h3>
+                  <li key={index}>
+                    <h3>{`Day ${index + 1}`}</h3>
                     <div>
-                      <div className='mb-2 block'>
-                        <label htmlFor='dayPlan'>Day Plan</label>
+                      <div className="mb-2 block">
+                        <label htmlFor="dayPlan">Day Plan</label>
                       </div>
                       <textarea
-                        className='w-full'
-                        id='dayPlan'
-                        name='dayPlan'
+                        className="w-full"
+                        id="dayPlan"
+                        name="dayPlan"
                         value={day.dayPlan}
-                        onChange={(event) =>
-                          handleFormChange(index, event)
-                        }></textarea>
+                        onChange={(event) => handleFormChange(index, event)}
+                      ></textarea>
                     </div>
                     <div>
-                      <div className='mb-2 block'>
-                        <label htmlFor='transport'>Transport</label>
+                      <div className="mb-2 block">
+                        <label htmlFor="transport">Transport</label>
                       </div>
                       <input
-                        className='w-full'
-                        id='transport'
-                        name='transport'
-                        type='text'
+                        className="w-full"
+                        id="transport"
+                        name="transport"
+                        type="text"
                         value={day.transport}
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </div>
+                    <PlacesSelector
+                      dayInputs={dayInputs}
+                      setDayInputs={setDayInputs}
+                      index={index}
+                      day={day}
+                      key={index}
+                    />
 
                     <div>
-                      <div className='mb-2 block'>
-                        <label htmlFor='country'>Country</label>
+                      <div className="mb-2 block">
+                        <label htmlFor="accomodation">Accomodation</label>
                       </div>
                       <input
-                        className='w-full'
-                        id='country'
-                        name='country'
-                        type='text'
-                        value={day.country}
-                        onChange={(event) => handleFormChange(index, event)}
-                      />
-                    </div>
-
-                    <div>
-                      <div className='mb-2 block'>
-                        <label htmlFor='region'>Region</label>
-                      </div>
-                      <input
-                        className='w-full'
-                        id='region'
-                        name='region'
-                        type='text'
-                        value={day.region}
-                        onChange={(event) => handleFormChange(index, event)}
-                      />
-                    </div>
-
-                    <div>
-                      <div className='mb-2 block'>
-                        <label htmlFor='place'>Place</label>
-                      </div>
-                      <input
-                        className='w-full'
-                        id='place'
-                        name='place'
-                        type='text'
-                        value={day.place}
-                        onChange={(event) => handleFormChange(index, event)}
-                      />
-                    </div>
-
-                    <div>
-                      <div className='mb-2 block'>
-                        <label htmlFor='accomodation'>Accomodation</label>
-                      </div>
-                      <input
-                        className='w-full'
-                        id='accomodation'
-                        name='accomodation'
-                        type='text'
+                        className="w-full"
+                        id="accomodation"
+                        name="accomodation"
+                        type="text"
                         value={day.accomodation}
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </div>
                     <Button onClick={() => removeDay(index)}>Remove Day</Button>
-                  </>
+                  </li>
                 );
               })}
             </ul>
             <Button onClick={addAnotherDay}>Add Another Day</Button>
 
-            <div className='w-full'>
-              <Button type='submit'>Submit</Button>
+            <div className="w-full">
+              <Button type="submit">Submit</Button>
             </div>
           </form>
         </div>
