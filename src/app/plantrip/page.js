5fa31cb002a0db48e0196d5af4d3906b44bfd7c3
Alex Straton <alexandrastraton1@gmail.com>
2024-08-12 14:10:06 +0100

    feat: adds min max inputs and styles the slider and select

--- src/app/plantrip/page.js
+++ src/app/plantrip/page.js
@@ -1,88 +1,88 @@
 "use client";
 import { useState } from "react";
-import { WithContext as ReactTags, SEPARATORS } from "react-tag-input";
+import CreatableSelect from "react-select/creatable";
 import ReactSlider from "react-slider";
 import { generateBudgetString } from "../utils/utils";
 
 export default function PlanYourTrip() {
   const [budget, setBudget] = useState(1);
-  const [tags, setTags] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [value, setValue] = useState([]);
 
-  // const handleChange = (event) => {
-  //   console.log(event.target.value);
-  //   setInput(event.target.value);
+  // const handleClick = (event) => {
+  //   console.log(event);
+  //   //setInput(event.target.value);
   // };
 
-  const handleAddition = (tag) => {
-    setTags((prevTags) => {
-      return [...prevTags, tag];
-    });
+  const handleKeyDown = (event) => {
+    if (!inputValue) return;
+    switch (event.key) {
+      case "Enter":
+      case "Tab":
+        setValue((prev) => [...prev, createOption(inputValue)]);
+        setInputValue("");
+        event.preventDefault();
+    }
   };
 
-  const handleDelete = (index) => {
-    setTags(tags.filter((_, i) => i !== index));
-  };
-
-  const handleClick = (event) => {
-    console.log(event);
-    //setInput(event.target.value);
-  };
-
-  const handleDrag = (tags, currPosition, newPosition) => {
-    console.log(tag);
-    const newTags = [...tags];
-    newTags.splice(currPosition, 1);
-    newTags.splice(newPosition, 0, tags);
-    // re-render
-    setTags(newTags);
-  };
+  const createOption = (label) => ({
+    label,
+    value: label,
+  });
 
   return (
-    <>
-      <h1 className='text-center text-3xl m-10'>Plan Your Trip</h1>
-      <div className='flex flex-row gap-6'>
-        <ReactTags
-          classNames={{
-            tags: "flex flex-wrap",
-            tagInput: "border p-1 rounded",
-            tag: "bg-blue-200 flex justify-center items-center m-1 rounded px-2",
-            remove: "bg-red-500 text-white p-1 ml-2 cursor-pointer rounded",
+    <section>
+      <h1 className='text-center text-3xl mx-auto'>Plan Your Trip</h1>
+      <div className='flex flex-row '>
+        <CreatableSelect
+          className='w-1/2 m-10 mx-auto'
+          components={{
+            DropdownIndicator: null,
           }}
-          tags={tags}
-          //suggestions={suggestions}
-          separators={[SEPARATORS.ENTER, SEPARATORS.COMMA]}
-          handleDelete={handleDelete}
-          handleAddition={handleAddition}
-          //handleDrag={handleDrag}
-          //onTagUpdate={onTagUpdate}
-          //inputFieldPosition="bottom"
-          //editable
-          //clearAll
-          //onClearAll={onClearAll}
-          maxTags={5}
+          inputValue={inputValue}
+          isClearable
+          isMulti
+          menuIsOpen={false}
+          onChange={(newValue) => setValue(newValue)}
+          onInputChange={(newValue) => setInputValue(newValue)}
+          onKeyDown={handleKeyDown}
+          placeholder='Type something and press enter...'
+          value={value}
         />
-        <button className='black_btn' onClick={handleClick}>
+        {/* <button className='black_btn' onClick={handleClick}>
           Search
-        </button>
-        <div>
-          <ReactSlider
-            id='slider'
-            className='horizontal-slider'
-            thumbClassName='example-thumb'
-            trackClassName='example-track'
-            onAfterChange={(newValue, thumbIndex) => {
-              setBudget(newValue);
-            }}
-            min={1}
-            max={3}
-            renderThumb={(props, state) => (
-              <div {...props} key={1}>
-                {generateBudgetString(state.valueNow, "$")}
-              </div>
-            )}
-          />
+        </button> */}
+      </div>
+
+      <div className='w-1/2 mx-auto'>
+        <h1>Choose your budget: </h1>
+        <ReactSlider
+          id='slider'
+          className='horizontal-slider'
+          thumbClassName='example-thumb'
+          trackClassName='example-track'
+          onAfterChange={(newValue, thumbIndex) => {
+            setBudget(newValue);
+          }}
+          min={1}
+          max={3}
+          renderThumb={(props, state) => (
+            <div {...props} key={1}>
+              {generateBudgetString(state.valueNow, "$")}
+            </div>
+          )}
+        />
+      </div>
+
+      <div className='flex flex-col'>
+        <h1 className='text-center'>Length of stay:</h1>
+        <div className='flex flex-row justify-center'>
+          <label>Min</label>
+          <input></input>
+          <label>Max</label>
+          <input></input>
         </div>
       </div>
-    </>
+    </section>
   );
 }
