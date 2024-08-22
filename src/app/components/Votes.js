c8167b88a94c47410bd6c8280252aebad086cb19
AGreaves99 <ajohngreaves@outlook.com>
2024-08-22 14:58:29 +0100

    feat: voting functionality

--- src/app/components/Votes.js
+++ src/app/components/Votes.js
@@ -1,64 +1,120 @@
 "use client";
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { postVote } from "../lib/data/itinerary_votes";
+import { checkVote, deleteVote, postVote } from "../lib/data/itinerary_votes";
+import {
+  FaRegThumbsDown,
+  FaRegThumbsUp,
+  FaThumbsDown,
+  FaThumbsUp,
+} from "react-icons/fa";
 
+export default function Votes({ itineraryInfo, session }) {
+  const userId = session?.user?.user_id;
+  const itineraryId = itineraryInfo.itinerary_id;
+  const actualVoteCount = itineraryInfo.total_votes;
+  const [displayedVoteCount, setDisplayedVoteCount] = useState(actualVoteCount);
+  const [upvote, setUpvote] = useState(false);
+  const [downvote, setDownvote] = useState(false);
 
-
-export default function Votes({userId, itineraryInfo, session}) {
-    const itineraryId = itineraryInfo.itinerary_id
-    const actualVoteCount = itineraryInfo.total_votes
-    const [voteValue, setVoteValue] = useState(0)
-    const [displayedVoteCount, setDisplayedVoteCount] = useState(actualVoteCount)
-    const [disabledButtons, setDisabledButtons] = useState([false,false]); //i.e. both buttons are initially not disabled
-   
-    useEffect(()=>{
-        if (session){
-            const writeVote = async ()=>{
-                await postVote(userId, itineraryId, voteValue)
-                console.log(voteValue, "<-- voteValue")
-            }       
-            writeVote()
+  useEffect(() => {
+    const getVoteValue = async () => {
+      if (session) {
+        const voteValue = await checkVote(session, itineraryId);
+        switch (voteValue) {
+          case 1:
+            setUpvote(true);
+            break;
+          case -1:
+            setDownvote(true);
+            break;
         }
-    },[displayedVoteCount, userId, itineraryId, voteValue])
+      }
+    };
+    getVoteValue();
+  }, []);
 
-    function handleClick(increment, button_index){
-        const incrementedVoteCount = displayedVoteCount + increment
-        setDisplayedVoteCount(incrementedVoteCount);
-        setVoteValue(increment)
-       
-        if (
-        (incrementedVoteCount > actualVoteCount && button_index === 0) ||
-        (incrementedVoteCount < actualVoteCount && button_index === 1)
-        ) {
-        setDisabledButtons(disabledButtons.map((_, i) =>i===button_index));
-        } else {
-        setDisabledButtons([false, false]);
+  async function handleUpvoteClick() {
+    try {
+      if (upvote) {
+        await deleteVote(session, itineraryId);
+        setUpvote(false);
+        setDisplayedVoteCount((currentVoteCount) => {
+          return currentVoteCount - 1;
+        });
+      } else if (downvote) {
+        await deleteVote(session, itineraryId);
+        setDownvote(false);
+        await postVote(userId, itineraryId, 1);
+        setUpvote(true);
+        setDisplayedVoteCount((currentVoteCount) => {
+          return currentVoteCount + 2;
+        });
+      } else {
+        await postVote(userId, itineraryId, 1);
+        setUpvote(true);
+        setDisplayedVoteCount((currentVoteCount) => {
+          return currentVoteCount + 1;
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  async function handleDownvoteClick() {
+    try {
+      if (downvote) {
+        await deleteVote(session, itineraryId);
+        setDownvote(false);
+        setDisplayedVoteCount((currentVoteCount) => {
+          return currentVoteCount + 1;
+        });
+      } else if (upvote) {
+        await deleteVote(session, itineraryId);
+        setUpvote(false);
+        await postVote(userId, itineraryId, -1);
+        setDownvote(true);
+        setDisplayedVoteCount((currentVoteCount) => {
+          return currentVoteCount - 2;
+        });
+      } else {
+        await postVote(userId, itineraryId, -1);
+        setDownvote(true);
+        setDisplayedVoteCount((currentVoteCount) => {
+          return currentVoteCount - 1;
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
   return (
     <div className="flex gap-4">
-    {session &&
-    <>
-      <button 
-      disabled = {disabledButtons[0]}
-      onClick = {()=>{handleClick(1,0)}}>
-        <Image alt="thumbs up" src="/assets/icons/thumbs up.jpg" width={30} height={30} />
-      </button>
-      <button 
-        disabled={disabledButtons[1]}
-        onClick = {()=>{handleClick(-1,1)}}>
-        <Image alt="thumbs down" src="/assets/icons/thumbs down.jpg" width={30} height={30} />
-      </button>
-    </>
-    }
-      <div className='flex items-center'>
-            <h2 className='text-lg font-semibold mr-2'>Votes:</h2>
-            <p className='text-lg text-gray-700'>
-              <span className='font-bold'>{displayedVoteCount}</span>
-            </p>
-          </div>
+      {session && (
+        <>
+          <button
+            onClick={() => {
+              handleUpvoteClick();
+            }}
+          >
+            {upvote ? <FaThumbsUp /> : <FaRegThumbsUp />}
+          </button>
+          <button
+            onClick={() => {
+              handleDownvoteClick();
+            }}
+          >
+            {downvote ? <FaThumbsDown /> : <FaRegThumbsDown />}
+          </button>
+        </>
+      )}
+      <div className="flex items-center">
+        <h2 className="text-lg font-semibold mr-2">Votes:</h2>
+        <p className="text-lg text-gray-700">
+          <span className="font-bold">{displayedVoteCount}</span>
+        </p>
+      </div>
     </div>
   );
 }
